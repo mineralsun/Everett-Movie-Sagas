3a8316deff8ab48cb09e6f5cf469160c7a263f34
Everett Butler <everettjamesbutler@gmail.com>
2023-05-05 13:49:00 -0500

    rough draft of code to get specific movie. It is not finding the specific film. Troubleshooting now

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -1,20 +1,35 @@
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 function DetailsPage() {
     const history = useHistory();
     const dispatch = useDispatch();
-    const film = useSelector(store => store.film);
+    const movies = useSelector(store => store.movies);
 
+    const getFilm = (id) => {
+        dispatch({ type: 'GET_FILM', payload: id });
+    }
 
     // This takes user back to home page where the list of movies is
     const returnToMovieList = (event) => {
         history.push('/');
     }
 
+    useEffect(() => {
+        getFilm();
+    }, []);
+
     return (
         <>
-        
+        {
+            movies.map(film => (
+                <div key={film.id}>
+                    <h4>{film.name}</h4>
+
+                </div>
+            ))
+        }
         <button onClick={returnToMovieList}>Home</button>
         </>
     ) ;
